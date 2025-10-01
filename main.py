6b6546edb51024755dcef36f487cff37202070a7
lamiye19 <ceciliagrumonde@gmail.com>
2025-10-01 18:11:07 +0000

    Sans NLU

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import streamlit as st
 from app.tab_chat import chatbot_tab
-from app.tab_nlu import nlu_tab
-#from app.tab_chat_nlu import chatbot_nlu_tab
+# from app.tab_nlu import nlu_tab
+# from app.tab_chat_nlu import chatbot_nlu_tab
 
 st.set_page_config(page_title="Assistant d'orientation", page_icon="")
 
-tab1, tab2 = st.tabs(["Chatbot RAG", "Compréhension (NLU)"])
-
-with tab1:
-    chatbot_tab()
+chatbot_tab()
+# tab1, tab2, tab3 = st.tabs(["Chatbot RAG", "Compréhension (NLU)", "RAG/NLU"])
+# with tab1:
+#     chatbot_tab()
     #st.markdown("Chat simple")
 
-with tab2:
-    nlu_tab()
+# with tab2:
+#     nlu_tab()
 #with tab3:
     #chatbot_nlu_tab()
     #st.markdown("Chat simple")
